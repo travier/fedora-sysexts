a287160304a6847fb4d2df34302414ea628dd8bc
Timothée Ravier <tim@siosm.fr>
2025-04-29 18:30:05 +0200

    workflow-templates: Build Silverblue & Kinoite specific sysexts

--- update_workflows.sh
+++ update_workflows.sh
@@ -28,18 +28,22 @@ main() {
     images=(
         'quay.io/fedora-ostree-desktops/base-atomic:41'
         'quay.io/fedora-ostree-desktops/base-atomic:42'
+        'quay.io/fedora-ostree-desktops/silverblue:41'
+        'quay.io/fedora-ostree-desktops/silverblue:42'
+        'quay.io/fedora-ostree-desktops/kinoite:41'
+        'quay.io/fedora-ostree-desktops/kinoite:42'
         'quay.io/fedora/fedora-coreos:stable'
         'quay.io/fedora/fedora-coreos:next'
     )
-    # 'quay.io/fedora-ostree-desktops/silverblue:41'
-    # 'quay.io/fedora-ostree-desktops/silverblue:42'
-    # 'quay.io/fedora-ostree-desktops/kinoite:41'
-    # 'quay.io/fedora-ostree-desktops/kinoite:42'
 
     # Set jobnames
     declare -A jobnames
     jobnames["quay.io/fedora-ostree-desktops/base-atomic:41"]="fedora-41"
     jobnames["quay.io/fedora-ostree-desktops/base-atomic:42"]="fedora-42"
+    jobnames["quay.io/fedora-ostree-desktops/silverblue:41"]="fedora-silverblue-41"
+    jobnames["quay.io/fedora-ostree-desktops/silverblue:42"]="fedora-silverblue-42"
+    jobnames["quay.io/fedora-ostree-desktops/kinoite:41"]="fedora-kinoite-41"
+    jobnames["quay.io/fedora-ostree-desktops/kinoite:42"]="fedora-kinoite-42"
     jobnames["quay.io/fedora/fedora-coreos:stable"]="fedora-coreos-stable"
     jobnames["quay.io/fedora/fedora-coreos:next"]="fedora-coreos-next"
 
